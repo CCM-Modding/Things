b0d74398004c13397d5b558c3aacee2211ee2bce
Captain Shadows <captain_shadows@aol.com>
2013-09-10 17:18:17 -0400

    found and fixed tht one :P

--- common/claycorp/wyldmod/entity/shroom/SpreadTarget.java
+++ common/claycorp/wyldmod/entity/shroom/SpreadTarget.java
@@ -1,35 +1,35 @@
 package claycorp.wyldmod.entity.shroom;
 
-import java.util.*;
-import net.minecraft.entity.ai.*;
+import java.util.Iterator;
+import java.util.List;
+
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.Vec3;
+import net.minecraft.entity.ai.EntityAIBase;
 
 public class SpreadTarget extends EntityAIBase
 {
     public EntityLiving me;
 
-    public SpreadTarget(EntityLiving entity)
+    public SpreadTarget(final EntityLiving entity)
     {
-        this.me = entity;
-        this.setMutexBits(0);
+        me = entity;
+        setMutexBits(0);
     }
 
     /**
      * Returns whether the EntityAIBase should begin execution.
      */
-    @SuppressWarnings("unchecked")
+    @Override
     public boolean shouldExecute()
     {
         return me.getAttackTarget() != null;
     }
 
-
     /**
      * Returns whether an in-progress EntityAIBase should continue executing
      */
+    @Override
     public boolean continueExecuting()
     {
         return false;
@@ -38,20 +38,21 @@ public class SpreadTarget extends EntityAIBase
     /**
      * Execute a one shot task or start executing a continuous task
      */
+    @Override
     public void startExecuting()
     {
         EntityLivingBase target = me.getAttackTarget();
 
         // Nothing to do, we just block the attack task.
-        List list = me.worldObj.getEntitiesWithinAABB(me.getClass(),
-                     me.boundingBox.expand(5.0D, 5.0D, 5.0D));
+        List<?> list = me.worldObj.getEntitiesWithinAABB(me.getClass(), me.boundingBox.expand(5.0D, 5.0D, 5.0D));
 
-        Iterator iterator = list.iterator();
+        Iterator<?> iterator = list.iterator();
 
-        while (iterator.hasNext()) {
-            EntityLiving friend = (EntityLiving)iterator.next();
+        while (iterator.hasNext())
+        {
+            EntityLiving friend = (EntityLiving) iterator.next();
 
-            if (me != friend && friend.getAttackTarget() == null && !friend.func_142014_c(target))
+            if ((me != friend) && (friend.getAttackTarget() == null) && !friend.isOnSameTeam(target))
             {
                 friend.setAttackTarget(target);
             }
@@ -61,6 +62,7 @@ public class SpreadTarget extends EntityAIBase
     /**
      * Updates the task
      */
+    @Override
     public void updateTask()
     {
         // Nothing to do, this is a one-shot task.
